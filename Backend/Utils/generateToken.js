d35cfb77067be64445fe71486e99f682161024c3
Harsshhhhiiii <harshit.student.min21@itbhu.ac.in>
2025-03-31 01:46:17 +0530

    remove security

--- Backend/Utils/generateToken.js
+++ Backend/Utils/generateToken.js
@@ -4,7 +4,7 @@ const generateTokenAndSetCookie = (username, res) => {
 	const token = jwt.sign({ username }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	});
-
+    console.log(token)
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, 
 		httpOnly: true, 
