16d9d316f15bb8bfd1a6d9f3e48eb7d6b3eacbbb
Harsshhhhiiii <harshit.student.min21@itbhu.ac.in>
2025-03-31 00:32:19 +0530

    done fixes

--- Frontend/src/Components/Logout.jsx
+++ Frontend/src/Components/Logout.jsx
@@ -1,7 +1,6 @@
-import useLogout from "../hooks/useLogout";
 import { toast } from "react-toastify";
 import { useState } from "react";
-
+import useLogout from "../Hooks/useLogout"
 
 const Logout = () => {
     const { logout } = useLogout();
