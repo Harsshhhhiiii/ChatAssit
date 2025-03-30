0bc2c2f99966b822b575d706d8c30901f7279cd8
Harsshhhhiiii <harshit.student.min21@itbhu.ac.in>
2025-03-31 01:42:54 +0530

    remove security

--- Backend/Utils/generateToken.js
+++ Backend/Utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (username, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, 
 		httpOnly: true, 
 		sameSite: "strict",
-		secure: process.env.NODE_ENV !== "development",
+		secure: false,
 	});
 };
 
